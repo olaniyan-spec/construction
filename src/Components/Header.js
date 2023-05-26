e09a8282b4af7e302ce2fef39ed454fd8cd1c8eb
Tofunmi <olaniyanolaoluwa3@gmail.com>
2023-05-26 23:03:27 +0100

    new commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,8 +11,8 @@ const Header = () => {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim non ac proin vitae massa. Mi mollis sed nunc hac sed sodales tristique erat sed non adipiscing ullamcorper. </p>
                 <button className='btn btn-white'>View Projects</button>
                 <button className='btn btn-black'>Contact Us</button>
-                </div> 
-        </div>
+            </div> 
+        
         <div className='header-card'>
             {
                 data.map((res, id)=>{
@@ -23,6 +23,7 @@ const Header = () => {
                     </Card>
                 })
             }
+        </div>
         </div>
     </header>
   )
--- src/Components/Newsletter.js
+++ src/Components/Newsletter.js
@@ -9,7 +9,7 @@ const Newsletter = () => {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim non ac proin vitae massa. Mi mollis sed nunc hac  sed sodales tristique erat sed non adipiscing ullamcorper. Ultricies orci hendrerit dui lacus vel quam massa.. </p>
             </div>
             <div className='newsletter-right'>
-                <form>
+                <form className='form-wrapper'>
                     <label/>Name:<br/>
                     <input type='text' placeholder='Enter Full Name'/><br/>
                     <label/>Email:<br/>
